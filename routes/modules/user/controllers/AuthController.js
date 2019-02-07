3b1ae76eca8c0a0d0f58a2440b02c31ff8cedb85
Thang Cao <thang.cao@logicsoftware.com.au>
2019-02-07 17:27:58 +0700

    AuthController

--- routes/modules/user/controllers/AuthController.js
+++ routes/modules/user/controllers/AuthController.js
@@ -2,16 +2,18 @@ var BaseController = require('../../../shared/controllers/BaseController');
 var ResponseBuilder = require('../../../shared/models/ResponseBuilder');
 
 // Define AuthController
-var AuthController =  new BaseController('auth');
+var AuthController = new BaseController('auth');
 
-AuthController.post('/signIn', function(req, res){
+AuthController.post('/signIn', function (req, res) {
     ResponseBuilder.onSuccess(res)
-                    .setMessage('SignIn successfully')
-                    .build();
+        .setMessage('SignIn successfully')
+        .build();
 });
 
-AuthController.post('/signUp', function(req, res){
-
+AuthController.post('/signUp', function (req, res) {
+    ResponseBuilder.onSuccess(res)
+        .setMessage('SignUp successfully')
+        .build();
 });
 
 module.exports = AuthController;

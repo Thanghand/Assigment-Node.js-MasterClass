e7d1a28b6b389bc9aa4af7d1186b160320dc8fcd
Thang Cao <thang.cao@logicsoftware.com.au>
2019-02-12 23:46:56 +0700

    Config https for project

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 // Dependencies
 var http = require('http');
+var https = require('https');
 var config = require('./config');
 var router = require('./routes/router');
+var fs = require('fs');
 
 // Configure the server to respond to all requests with a string
-var server = http.createServer(function (req, res) {
-  router.handleRequest(req, res);
+// var httpServer = http.createServer(function (req, res) {
+//   router.handleRequest(req, res);
+// });
+
+// // Start the server
+// httpServer.listen(config.httpPort, function () {
+//   console.log('The http server is up and running on port ' + config.httpPort + ' in ' + config.envName + ' mode.');
+//   router.build();
+// });
+
+var serverOptions = {
+	'key': fs.readFileSync('./https/key.pem'),
+	'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(serverOptions, function (req, res) {
+	console.log('Ahihih');
+ 	router.handleRequest(req, res);
 });
 
-// Start the server
-server.listen(config.port, function () {
-  console.log('The server is up and running on port ' + config.port + ' in ' + config.envName + ' mode.');
+httpsServer.listen(config.httpsPort, function () {
+  console.log('The https server is up and running on port ' + config.httpsPort + ' in ' + config.envName + ' mode.');
   router.build();
 });
 
